06b2fcedf27fcbb22c38cb5deea746322380afd2
prafu <prafu@LAPTOP-AHR9SHF8>
2022-08-15 18:52:49 +0530

    Abstraction

---
+++ C2TC_Core_Java/src/com/tns/core/AbstractDemo.java
@@ -0,0 +1,30 @@
+package com.tns.core;
+
+public class AbstractDemo {
+
+	public static void main(String[] args) {
+
+		Scooty Activa=new Scooty();
+			Activa.NoOfWheels();
+
+	}
+
+}
+abstract class Vehicle   //partially implemented class
+{
+	public abstract void NoOfWheels();
+	
+	public abstract void m();
+
+
+	
+}
+
+ abstract class Scooty extends Vehicle
+{
+	public void NoOfWheels()
+	{
+		System.out.println(2);
+	}
+}
+ 
